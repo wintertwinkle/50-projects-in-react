1a1ad68805558353b0f287c7c91b1498cbbdd6d1
wintertwinkle <1070872423@qq.com>
2022-03-19 15:40:20 +0800

    add comment in rotating-navigation/index.js

--- src/components/rotating-navigation/index.js
+++ src/components/rotating-navigation/index.js
@@ -5,12 +5,22 @@ import Nav from "./nav"
 
 import image from "./images/oie_2195929gQPjiDAI.jpg"
 
-// Question: when there are multiple levels of component hierachcies
+// Question: When there are multiple levels of component hierachcies
 // one deep nested component need a prop from a top level component
 // but the props need to pass over every single component in the path from the origin component to the target component
 // Is there some possible ways to solve this problem?
 
 // Answer: `React Context` realted API: `React.reacteContext`
+
+/**
+ * Update: Context is primarily used when some data needs to be accessible by many components
+ * at different nesting levels.
+ * Use `React Context` will cause components reuse more difficult
+ *
+ * If you only want to avoid passing some props through many levels
+ * `Component composition` [https://reactjs.org/docs/composition-vs-inheritance.html]
+ * is ofen a simpler solution than context
+ */
 
 export default class RotatingNavigation extends Component {
   constructor(props) {
